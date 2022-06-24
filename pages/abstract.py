f5575d068203efb6ebf2109821d14724532d70e3
cdlavacudeg <cristianlav10@gmail.com>
2022-06-24 10:54:42 -0500

    [Update] Home content - add 3 maps and slider

--- pages/abstract.py
+++ pages/abstract.py
@@ -2,30 +2,74 @@ from pydoc import classname
 from dash import html, dcc, Input, Output, callback
 from utils import violencia
 
-mapaViolencia = violencia.map_suicides_per_year_population(2016)
-mapaViolencia.save("data/violencia/mapa_suicidios.html")
-mapContainer = html.Div(
-    html.Iframe(
-        srcDoc=open("data/violencia/mapa_suicidios.html", "r").read(),
-        width="100%",
-        height="300",
-        id="map-v",
-    ),
+slider = dcc.Slider(
+    2016,
+    2019,
+    0,
+    marks={2016: "2016", 2017: "2017", 2018: "2018", 2019: "2019"},
+    value=2016,
+    id="year-slider",
+)
+principalMap = html.Div(
+    [
+        html.H2("Suicides map for year"),
+        html.Iframe(
+            srcDoc=open("data/violencia/mapa_suicidios.html", "r").read(),
+            width="100%",
+            height="300",
+            id="map-principal",
+        ),
+        slider,
+    ],
     className="card",
     id="map-abstract",
 )
-fig1Container = html.Div(className="card", id="fig1-abstract")
-fig2Container = html.Div(className="card", id="fig2-abstract")
 
-layout = [mapContainer, fig1Container, fig2Container]
+secondaryMap = html.Div(
+    [
+        html.H2("Violence map for year"),
+        html.Iframe(
+            srcDoc=open("data/violencia/mapa_violencia.html", "r").read(),
+            width="100%",
+            height="300",
+            id="map-secondary",
+        ),
+    ],
+    className="card",
+    id="map-secondary",
+)
 
-# @callback(
-#     Output("sidebar-id", "className"),
-#     Output("container-id", "className"),
-#     Input("btn", "n_clicks"),
-# )
-# def callback(n_clicks, current_classes):
-#     print(n_clicks, current_classes)
-#     if "active" in current_classes:
-#         return "sidebar", "container"
-#     return ("sidebar" + " active", "container" + " active")
+tertiaryMap = html.Div(
+    [
+        html.H2("Suicide attempts  map for year"),
+        html.Iframe(
+            srcDoc=open("data/violencia/mapa_intentos_suicidio.html", "r").read(),
+            width="100%",
+            height="300",
+            id="map-tertiary",
+        ),
+    ],
+    className="card",
+    id="fig2-abstract",
+)
+
+layout = [principalMap, secondaryMap, tertiaryMap]
+
+
+@callback(
+    Output("map-principal", "srcDoc"),
+    Output("map-secondary", "srcDoc"),
+    Output("map-tertiary", "srcDoc"),
+    Input("year-slider", "value"),
+)
+def callback(value):
+    mapaSuicidios = violencia.map_suicides_per_year_population(value)
+    mapaSuicidios.save("data/violencia/mapa_suicidios.html")
+    srcPrincipal = open("data/violencia/mapa_suicidios.html", "r").read()
+    mapaViolencia = violencia.map_suicides_per_year_population(value)
+    mapaViolencia.save("data/violencia/mapa_violencia.html")
+    srcSecond = open("data/violencia/mapa_violencia.html", "r").read()
+    mapaIntentos = violencia.map_suicides_per_year_population(value)
+    mapaIntentos.save("data/violencia/mapa_intentos_suicidio.html")
+    srcThird = open("data/violencia/mapa_intentos_suicidio.html", "r").read()
+    return (srcPrincipal, srcSecond, srcThird)
--- utils/violencia.py
+++ utils/violencia.py
@@ -84,18 +84,13 @@ def map_violencia_per_year_population(year):
 
 def map_suicides_per_year_population(year):
     df = pd.read_csv("data/violencia/MERGED-SuicidiosPopulationGeo.csv")
-    # df.drop(columns=["CODE_DPTO", "CODE_MUNICIPIO"], inplace=True) # NOT NEED SINCE I AM KEEPING ONLY LAT AND LONG
     df = df[df["YEAR"] == year]
-    # print(df.head())
     # Heat map for count of begin location
     START_COORDS = [4.7110, -74.0721]
     map_aux = folium.Map(location=START_COORDS, zoom_start=5)
-    # Create and clean the heat dataframe
     heat_df = df[["LATITUD", "LONGITUD"]].dropna()
-    # Create the list of lists
     heat_df = [
         [row["LATITUD"], row["LONGITUD"]] for index, row in heat_df.iterrows()
-    ]  # THIS IS SLOW!
-    # Add the data to the map and plot
+    ]
     HeatMap(heat_df, radius=10, blur=15, control=True).add_to(map_aux)
     return map_aux
